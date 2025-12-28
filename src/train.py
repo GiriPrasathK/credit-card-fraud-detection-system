7265ab45c6448b18e79e4e5ea0d5f1adcd26cba0
{GIRIPRASATHK} <{tn33af1332@gmail.com}>
2025-12-28 18:56:38 +0530

    Modified Directory paths

--- src/train.py
+++ src/train.py
@@ -4,9 +4,8 @@ import xgboost as xgb
 import os 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_PATH = os.path.join(BASE_DIR, "data", "raw", "creditcard.csv")
-
-MODEL_PATH='models/fraud_xgb.pkl'
-SCALER_PATH='models/scaler.pkl'
+MODEL_PATH = os.path.join(BASE_DIR, "models", "fraud_xgb.pkl")
+SCALER_PATH = os.path.join(BASE_DIR, "models", "scaler.pkl")
 
 def train():
     X_train, X_test, Y_train, Y_test,scaler=preprocess_data(DATA_PATH)
@@ -22,6 +21,7 @@ def train():
 )
 
     model.fit(X_train,Y_train)
+    os.makedirs(os.path.dirname(MODEL_PATH), exist_ok=True)
 
     joblib.dump(model,MODEL_PATH)
     joblib.dump(scaler,SCALER_PATH)
